5d5fb4805e7692efa4289f9fcbdde5cdbb022982
leonardosibela <leonardo_sibela@hotmail.com>
2018-02-23 22:36:43 +0000

    Esqueleto do projeto

---
+++ ProjetoFilmes/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ProjetoFilmes
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

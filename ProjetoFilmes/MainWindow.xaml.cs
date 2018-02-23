64f7ba8f0faf6ccb7ce85989bc49e61985cb4e6c
leonardosibela <leonardo_sibela@hotmail.com>
2018-02-23 23:12:28 +0000

    Inserindo cadastro de filmes

--- ProjetoFilmes/MainWindow.xaml.cs
+++ ProjetoFilmes/MainWindow.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 
 namespace ProjetoFilmes
 {
     public partial class MainWindow : Window
     {
+        private List<Movie> movies = new List<Movie>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -14,6 +18,18 @@ namespace ProjetoFilmes
             txtName.Text = "";
             txtGenre.Text = "";
             txtDirector.Text = "";
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            string genre = txtGenre.Text;
+            string director = txtDirector.Text;
+            
+            movies.Add(new Movie(name, genre, director));
+            movies = movies.OrderBy(m => m.Name).ToList();
+            dtgMovies.ItemsSource = movies;
+
         }
     }
 }

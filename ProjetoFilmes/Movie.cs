dad9f7d1f03b99a7d6f34cb223804e0adbed70f7
leonardosibela <leonardo_sibela@hotmail.com>
2018-02-23 22:56:13 +0000

    Adicionando classe de filmes

---
+++ ProjetoFilmes/Movie.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjetoFilmes
+{
+    class Movie
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+
+        public Movie(string name, string genre, string director)
+        {
+            Name = name;
+            Genre = genre;
+            Director = director;
+        }
+    }
+}

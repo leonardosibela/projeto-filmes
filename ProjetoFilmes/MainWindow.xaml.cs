71e3ab211cd7a565be37604a94f8a66e01b8df5a
leonardosibela <leonardo_sibela@hotmail.com>
2018-02-23 22:50:51 +0000

    Criando evento para limpar campos

--- ProjetoFilmes/MainWindow.xaml.cs
+++ ProjetoFilmes/MainWindow.xaml.cs
@@ -8,5 +8,12 @@ namespace ProjetoFilmes
         {
             InitializeComponent();
         }
+
+        private void btnClean_Click(object sender, RoutedEventArgs e)
+        {
+            txtName.Text = "";
+            txtGenre.Text = "";
+            txtDirector.Text = "";
+        }
     }
 }
